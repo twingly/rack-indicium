69416c350a50d533098c00f3a199d315b5eef4a4
Johan Eckerström <johan@duh.se>
2015-03-27 16:59:15 +0100

    Integration specs with Sinatra and Grape
    Closes #1

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -10,4 +10,15 @@ module Helpers
   def valid_jwt_payload
     { "exp" => Time.now.to_i + 10, "custom" => 1337 }
   end
+
+  def authorization_request_header(secret, extra_contents = {})
+    contents = {
+      "aud" => "some_id",
+      "sub" => "user_id",
+    }.merge(extra_contents)
+
+    jwt = JWT.encode(contents, secret)
+
+    header "Authorization", "Bearer #{jwt}"
+  end
 end
